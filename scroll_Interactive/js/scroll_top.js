bd1d551501d10636a8686f44d43861ee7908d375
corn1200 <corn120033@gmail.com>
2020-12-31 09:43:45 +0900

    feat: 가로 진행 바를 구현한다

---
+++ scroll_Interactive/js/scroll_top.js
@@ -0,0 +1,14 @@
+let scrollTop = 0
+let bar
+
+window.onload = function() {
+    bar = document.getElementsByClassName("bar")[0]
+}
+
+window.addEventListener("scroll", function(e) {
+    scrollTop = document.documentElement.scrollTop
+    let per = Math.ceil(scrollTop / (document.body.scrollHeight - window.outerHeight) * 100)
+    console.log(per)
+
+    bar.style.width = per + "%"
+}, false)
