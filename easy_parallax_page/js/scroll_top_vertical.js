54796291c81a11e7dc7d9a8915ed80100f242990
corn1200 <corn120033@gmail.com>
2020-12-31 10:21:31 +0900

    feat: 구름 이미지가 천천히 움직이는 패럴랙스 효과를 구현한다

--- easy_parallax_page/js/scroll_top_vertical.js
+++ easy_parallax_page/js/scroll_top_vertical.js
@@ -1,14 +1,17 @@
 let scrollTop = 0
 let bar
+let cloudWrap
 
 window.onload = function() {
     bar = document.getElementsByClassName("bar")[0]
+    cloudWrap = document.getElementsByClassName("cloudWrap")[0]
 }
 
 window.addEventListener("scroll", function(e) {
     scrollTop = document.documentElement.scrollTop
     let per = Math.ceil(scrollTop / (document.body.scrollHeight - window.outerHeight) * 100)
-    console.log(per)
 
     bar.style.height = per + "%"
+
+    cloudWrap.style.transform = "translate(0, " + scrollTop / 1.2 + "px)"
 }, false)
